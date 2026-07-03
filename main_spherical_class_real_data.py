9566709b47f9ac925caa1ba5634d42dd74cb0b24
n_masala <n.masala5@studenti.unica.it>
2026-07-03 10:37:04 +0200

    03/07/2026

--- main_spherical_class_real_data.py
+++ main_spherical_class_real_data.py
@@ -14,8 +14,7 @@ from New_Spherical_Class_class import New_Spherical_Classifier
 from New_Helper_SC import *
 from sklearn.metrics import accuracy_score, f1_score
 
-#data_list = ['liver','blood_transfusion','flowmeters','heart','diabetes','breast','divorce','australian','Mesothelioma','Gallstone','sonar','breast_wisconsin','germannumer','HillValley_training','Fertility']
-data_list = ['HillValley_training','Fertility']
+data_list = ['liver','blood_transfusion','flowmeters','heart','diabetes','breast','divorce','australian','Mesothelioma','Gallstone','sonar','breast_wisconsin','germannumer','HillValley_training','Fertility']
 
 for d in data_list:
     X = np.genfromtxt('datasets/'+d+'_data.csv',delimiter=',')
