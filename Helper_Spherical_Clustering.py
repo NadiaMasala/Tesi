705d04aff139e193c63f40232f25b2c0d7c0a75e
NadiaMasala <n.masala5@studenti.unica.com>
2026-07-17 11:38:26 +0200

    17/07/2026

--- Helper_Spherical_Clustering.py
+++ Helper_Spherical_Clustering.py
@@ -38,37 +38,45 @@ def spherical_clustering_fit(X,l,d):
 
 # Sliding Window Algorithm for density regions along the real line
 def sliding_window(x,l,d):
-    n_regions = 0
-    start = 0
-    n_iters = 0
-    regions = []
-    outliers = []
+    n_regions = 0  # number of dense regions
+    start = 0  # starting index
+    n_iters = 0  # number of iterations
+    dense = 0  # flag is 1 if the last point is added to a dense region, 0 otherwise
+    regions = []  # list of dense and no dense (empty) regions
+    outliers = []  # list of outliers
     while start+l-1 <= len(x)-1:
         window = x[start:start+l-1]
-        distances = []
+        distances = {}
         for j in range(len(window)):
             for k in range(len(window)):
-                if k!=j:
-                    distances.append(np.linalg.norm(window[j] - window[k]))
-        d_max = max(distances)
-        if d_max <= d:
-            if n_iters == 0:
-                regions[n_iters] = regions[n_iters].append(window)
-                n_regions += 1
-                start += 1
-            elif len(regions[n_iters-1]) > 0:  # if the previous region is not empty (is a dense region)
-                # add the last point of the current window in the previous dense region
-                regions[n_iters-1] = regions[n_iters-1].append(window[-1])
-                start += 1  # slide
-            else: # the previous region is empty (is not dense) or does not exist
+                if k < j:
+                    distances[j].append(np.linalg.norm(window[j] - window[k]))
+        d_max_list = []
+        for d_list in distances.values():
+            d_max = max(d_list)
+            d_max_list.append(d_max)
+        d_max_all = max(d_max_list)
+        if d_max_all <= d:
+            if dense == 0:  # if we are at the first iteration or the previous region is not dense
                 # define a new dense region with the points of the current window
                 regions[n_iters] = regions[n_iters].append(window)
                 n_regions += 1
-                start += 1
-        elif d_max > d:
-            if n_iters > 0:
-                if not len(regions[n_iters-1]) > 0:
-                    outliers.append(start)
+                start += 1  # slide
+                dense = 1
+            elif dense == 1:  # if at the previous iteration the point was added to a dense region
+                # add the last point of the current window in the last dense region found
+                last = 0
+                for i in range(n_iters):
+                    if len(regions[i])>0:
+                        last = i
+                regions[last] = regions[last].append(window[-1])
+                start += 1  # slide
+                dense = 1
+        elif d_max_all > d:
+            if dense == 0:  # if we are at the first iteration or the previous region is not dense
+                outliers.append(window[0:-2])
+            elif dense == 1:  # if at the previous iteration we had a dense region
+                dense = 0
             start = start+l-1
         n_iters += 1
 
